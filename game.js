7b958a62f0f9f61d1bc3419be3cf3e05209dd1d5
Ramzi Dreessen <ramzidreessen@gmail.com>
2018-03-04 23:29:06 -0600

    fixed bugs

--- game.js
+++ game.js
@@ -2,8 +2,6 @@ function init() {
     startGame();
 }
 
-
-
 function startGame() {
 
     let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -11,13 +9,18 @@ function startGame() {
     
     let userWins = 0;
     let userLosses = 0;
+
+    let userGuess = "";
+    let userGuessed = [];
+    let userGuessesLeft = 10;
     
     function resetGame() {
-        let userGuess = "";
-        let userGuessed = [];
-        let userGuessesLeft = 10;
+        userGuess = "";
+        userGuessed = [];
+        userGuessesLeft = 10;
     
         document.getElementById("guessesLeft").textContent = userGuessesLeft;
+        document.getElementById("guessesSoFar").textContent = userGuessed;
     
         computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
         console.log("Computer letter is " + computerChoice);
@@ -30,15 +33,16 @@ function startGame() {
     // do things when keys are pressed
     document.onkeyup = function(event) {
         userGuess = event.key;
+        console.log("guesses left: " + userGuessesLeft);
         
         if (userGuess === computerChoice) {
             userWins++;
             document.getElementById("wins").textContent = userWins;
 
-            alert("You win!");
+            alert("You win, the letter was " + computerChoice + " !");
             resetGame();
 
-        } else if ((userGuess !== computerChoice) && (userGuessesLeft > 1) && (userGuessed.indexOf(userGuess) > -1)) {
+        } else if ((userGuess !== computerChoice) && (userGuessesLeft > 1) && !(userGuessed.indexOf(userGuess) > -1)) {
             userGuessesLeft--;
             document.getElementById("guessesLeft").textContent = userGuessesLeft;
 
@@ -56,7 +60,6 @@ function startGame() {
     };
 
 }
-
 
 // initialize the game
 init();
